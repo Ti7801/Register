75c587d635e6d785474a023bc82afc41e70821ad
Ti7801 <tiagodaltro19@gmail.com>
2025-06-23 19:40:51 -0300

    Criação da estrutura do projeto para cadastro de usuários. Organização do IdentityUser na Model Usuário.

---
+++ src/BibliotecaData/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaData.Data
+{
+    public class AppDbContext : IdentityDbContext<Usuario, IdentityRole<Guid>, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ src/Register/Controllers/CadastrarUsuario.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Register.Controllers
+{
+    [ApiController]
+    [Route("api/cadastrarUsuario")]
+    public class CadastrarUsuario : ControllerBase
+    {
+    }
+}
