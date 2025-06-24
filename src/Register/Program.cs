3dcb543f66da543e99feb995e1b18377484c43be
Ti7801 <tiagodaltro19@gmail.com>
2025-06-24 14:47:30 -0300

    Configurando a conexão do banco de dados e injetando o serviço de injeção de dependência para IdentityUser.

--- src/Register/Program.cs
+++ src/Register/Program.cs
@@ -1,4 +1,36 @@
+using BibliotecaBusiness.Models;
+using BibliotecaData.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Conexão com o PostgreSQL
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Configuração do Identity
+
+builder.Services.AddIdentity<Usuario, IdentityRole<Guid>>(options =>
+{
+    //Configuração de senha
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+
+    //Configuração de Lockout - Bloqueio após várias tentativas
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true; // Bloqueio automatico para novos usuários
+
+    //Configuração de usuário
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AppDbContext>()
+  .AddDefaultTokenProviders();
+
 
 // Add services to the container.
 
@@ -6,11 +38,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-// Injetando as configurações do Banco de Dados
-
-
 
 var app = builder.Build();
 
@@ -23,6 +50,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
